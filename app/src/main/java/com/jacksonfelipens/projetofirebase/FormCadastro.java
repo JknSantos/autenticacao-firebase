322f7b66d4294f5d711ca0c874285a0eed754ae6
Jackson Felipe do Nascimento Santos <jacksonfelipe.contact@gmail.com>
2025-08-14 22:12:24 -0300

    Implementado metodo CadastrarUsuario no FormCadastro, ajuste de id e de build

--- app/src/main/java/com/jacksonfelipens/projetofirebase/FormCadastro.java
+++ app/src/main/java/com/jacksonfelipens/projetofirebase/FormCadastro.java
@@ -1,14 +1,27 @@
 package com.jacksonfelipens.projetofirebase;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FormCadastro extends AppCompatActivity {
+
+    private EditText edit_nome, edit_email, edit_senha;
+    private Button button_cadastrar;
+
+    String[] mensagens = {"Preencha todos os campos!", "Cadastro realizado com sucesso!", "Erro ao cadastrar usuário!"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +30,53 @@ public class FormCadastro extends AppCompatActivity {
         setContentView(R.layout.activity_form_cadastro);
 
         getSupportActionBar().hide();
+        IniciarComponentes();
+
+        button_cadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nome = edit_nome.getText().toString();
+                String email = edit_email.getText().toString();
+                String senha = edit_senha.getText().toString();
+
+                if (nome.isEmpty() || email.isEmpty() || senha.isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(view,mensagens[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                } else {
+                    CadastrarUsuario(view);
+                }
+            }
+        });
+
+
+    }
+
+    private void CadastrarUsuario(View view) {
+
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if (task.isSuccessful()) {
+                    Snackbar snackbar = Snackbar.make(view, mensagens[1], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+
+                }
+            }
+        });
+    }
+
+    private void IniciarComponentes() {
+        edit_nome = findViewById(R.id.text_cadastro_nome);
+        edit_email = findViewById(R.id.text_cadastro_email);
+        edit_senha = findViewById(R.id.text_cadastro_senha);
+        button_cadastrar = findViewById(R.id.btn_cadastrar);
     }
 }
